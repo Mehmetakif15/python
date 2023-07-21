8d36de2cbf52547941c0c9abb60f5fb56eeec940
mehmet akif <macguld15@gmail.com>
2023-07-21 06:44:21 +0300

    pagi-filter-searc-permission-auth

---
+++ 15-PaginationFilterSearch/todo/paginations.py
@@ -0,0 +1,16 @@
+from rest_framework.pagination import (PageNumberPagination, LimitOffsetPagination, CursorPagination)
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 25
+    page_size_query_param = 'adet'
+    page_query_param = 'sayfa'
+
+class CustomLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 25
+    limit_query_param = 'adet'
+    offset_query_param = 'baslangic'
+
+class CustomCursorPagination(CursorPagination):
+    page_size = 25
+    cursor_query_param = 'imlec'
+    ordering = 'id'
